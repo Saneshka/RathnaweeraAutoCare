4e184523042cc767d7985c7a8e90d210206804de
saneshka <costasaneshka@outlook.com>
2024-10-29 16:47:58 +0530

    create autocaretimetable

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -216,6 +216,11 @@ const routes: Routes = [
     data: { pageTitle: 'Hoisttypes' },
     loadChildren: () => import('./hoisttype/hoisttype.routes'),
   },
+  {
+    path: 'autocaretimetable',
+    data: { pageTitle: 'Autocaretimetables' },
+    loadChildren: () => import('./autocaretimetable/autocaretimetable.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
